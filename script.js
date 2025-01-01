be259f62a01ad3d20406b4df19ec0d447d2f10c9
Aimee So <44697323+aimeeso@users.noreply.github.com>
2025-01-02 07:35:53 +0800

    Added more projects

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ function showTab(tabName) {
   // Hide all tab contents
   const contents = document.querySelectorAll('.tab-content');
   contents.forEach(content => {
-      content.classList.add('hidden');
-      content.classList.remove('active'); // Remove the active class from all tabs
+    content.classList.add('hidden');
+    content.classList.remove('active'); // Remove the active class from all tabs
+  });
+
+  // Show the selected tab content
+  const activeTab = document.getElementById(tabName);
+  activeTab.classList.remove('hidden');
+  activeTab.classList.add('active');
+}
+
+function showAcTab(tabName) {
+  // Hide all tab contents
+  const contents = document.querySelectorAll('.ac-tab-content');
+  contents.forEach(content => {
+    content.classList.add('hidden');
+    content.classList.remove('active'); // Remove the active class from all tabs
   });
 
   // Show the selected tab content
@@ -16,8 +30,8 @@ function showNestedTab(nestedTabName) {
   // Hide all nested tab contents
   const nestedContents = document.querySelectorAll('.nested-tab-content');
   nestedContents.forEach(content => {
-      content.classList.add('hidden');
-      content.classList.remove('active'); // Remove the active class from all nested tabs
+    content.classList.add('hidden');
+    content.classList.remove('active'); // Remove the active class from all nested tabs
   });
 
   // Show the selected nested tab content
@@ -31,14 +45,21 @@ document.addEventListener('DOMContentLoaded', () => {
   // Activate the first tab
   const firstTab = document.querySelector('.tab-content');
   if (firstTab) {
-      firstTab.classList.remove('hidden');
-      firstTab.classList.add('active');
+    firstTab.classList.remove('hidden');
+    firstTab.classList.add('active');
+  }
+
+  // Activate the first ac tab
+  const acFirstTab = document.querySelector('.ac-tab-content');
+  if (acFirstTab) {
+    firstTab.classList.remove('hidden');
+    firstTab.classList.add('active');
   }
 
   // Activate the first nested tab
   const firstNestedTab = document.querySelector('.nested-tab-content');
   if (firstNestedTab) {
-      firstNestedTab.classList.remove('hidden');
-      firstNestedTab.classList.add('active');
+    firstNestedTab.classList.remove('hidden');
+    firstNestedTab.classList.add('active');
   }
 });
